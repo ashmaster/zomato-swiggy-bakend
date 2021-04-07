907e86a25a05d6c812b5ed7797793f07a7301af7
Ashish <ashishmp0471@gmail.com>
2021-04-07 20:12:40 +0530

    Fix

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ app.get('/getZomDets', async (req,res)=>{
 })
 
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT)
 
 console.log("Api running on Heroku")
