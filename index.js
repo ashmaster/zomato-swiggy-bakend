ea4379e8e9c3b75699b585f243bd0f6a5426cd2d
Ashish <ashishmp0471@gmail.com>
2021-04-07 17:30:54 +0530

    Fix

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     next();
   });
-app.use(cors())
+app.use(cors({credentials:true}))
 
 
 app.get('/getFood',async (req,res) => {
